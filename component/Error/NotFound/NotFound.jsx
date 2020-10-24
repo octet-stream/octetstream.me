c01a34f080830c5128285dca8f9ee68c25dc3dc1
Nick K <nick.kruchinin@gmail.com>
2020-10-24 11:58:49 +0300

    Add custom 404 page

--- component/Error/NotFound/NotFound.jsx
+++ component/Error/NotFound/NotFound.jsx
@@ -1,4 +1,38 @@
-/** @type {React.FC} */
-const NotFound = () => <div>Page not found</div>
+import {Fragment} from "react"
+
+import Link from "next/link"
+
+import Title from "component/Title"
+
+import {
+  container,
+  content,
+  message,
+  status
+} from "./not-found.module.css"
+
+// TODO: Improve page design
+/**
+ * @type {React.FC}
+ */
+const NotFound = () => (
+  <Fragment>
+    <Title title="Page not found" />
+
+    <div className={container}>
+      <div className={content}>
+        <div className={message}>
+          <h1 className={status}>
+            404
+          </h1>
+
+          <div>
+            This page could not be found.
+          </div>
+        </div>
+      </div>
+    </div>
+  </Fragment>
+)
 
 export default NotFound
