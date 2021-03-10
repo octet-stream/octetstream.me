6a9638354d525512d5f9b6c0b732af1f165dc869
Nick K <nick.kruchinin@gmail.com>
2021-03-10 19:56:49 +0300

    Bump next.js to fix the issue with preact integration

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 const withPreact = require("next-plugin-preact")
 
-module.exports = withPreact({
-  future: {
-    webpack5: true
-  },
-  experimental: {
-    modern: true,
-  },
-})
+module.exports = withPreact()
